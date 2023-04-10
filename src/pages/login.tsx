b86b756c845d368e5aaf153665959c023c2a8ada
j1-dev <aephyxen@gmail.com>
2023-04-10 23:51:49 +0200

    APP_ROOT comment

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,7 +8,7 @@ import classNames from "classnames";
 import styles from "./Home.module.css";
 import Header from "../components/Header";
 
-const APP_ROOT = "http://localhost:3000/";
+const APP_ROOT = "http://localhost:3000/"; // Change this depending on where you want to be redirected after the email
 
 export default function Login() {
   const [email, setEmail] = useState("");
