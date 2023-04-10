a2e9c808e2c3d2e55d1ed00ba7184cb308bf93ce
j1-dev <aephyxen@gmail.com>
2023-04-10 23:54:41 +0200

    APP_ROOT rollback

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -8,7 +8,7 @@ import classNames from "classnames";
 import styles from "./Home.module.css";
 import Header from "../components/Header";
 
-const APP_ROOT = "http://localhost:3000/"; // Change this depending on where you want to be redirected after the email
+const APP_ROOT = "https://intelli-pic.vercel.app/"; // Change this depending on where you want to be redirected after the email
 
 export default function Login() {
   const [email, setEmail] = useState("");
